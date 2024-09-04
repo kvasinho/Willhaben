c7862551e759cceeef1089cbca4c08147b8a2393
kvasinho <kvasinho@googlemail.com>
2024-09-04 21:47:24 +0200

    Fixed Bugs, edited CLI Command. Create working to jsonformat

---
+++ Willhaben.Presentation/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Spectre.Console;
+using Spectre.Console.Cli;
+using Willhaben.Presentation.Commands;
+
+
+namespace Willhaben.Presentation
+{
+    class Program
+    {
+
+        public  static int  Main(string[] args)
+        {
+            //Commands
+            //- Create: Creates a new scraper 
+            // Edit: Edits an existing scraper 
+            //Delete: Removes a scraper 
+            //Run: Runs all scrapers 
+            var app = new CommandApp();
+            app.Configure(config =>
+            {
+                config.AddCommand<CreateCommand>("create");
+            });
+
+            return app.Run(args);
+
+            
+        }
+    }
+}
